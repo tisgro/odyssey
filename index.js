81b0c456930fc8f56fcb6e2d9604ccf48fe9e286
Travis Isgro <tisgro@saffron.net>
2019-02-07 22:42:37 +0100

    update response

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .get('/', (req, res) => res.send('Hello'))
+  .get('/', (req, res) => res.send('Hello World'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
