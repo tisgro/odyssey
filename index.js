cac3282e424bcc1545bec00f4b38ef7a61d59af8
Travis Isgro <tisgro@saffron.net>
2019-02-07 23:20:01 +0100

    victory lap

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .get('/', (req, res) => res.send('Hello World\n'))
+  .get('/', (req, res) => res.send('Hello World!\n'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
